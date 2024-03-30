5165edd033ce1931ed78632782aa498082952838
sunghee119z@naver.com <sunghee119z@naver.com>
2024-03-30 23:41:23 +0900

    유저 로그인 테스트

--- src/test/java/com/my/user/UserTest.java
+++ src/test/java/com/my/user/UserTest.java
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.RequestBuilder;
@@ -59,7 +60,17 @@ public class UserTest {
 
     @Test
     @DisplayName("유저 로그인 성공")
-    void userLogin() {
+    void userLogin() throws Exception{
+        //given
+        UserLoginDto userLoginDto = new UserLoginDto(id , pw);
+        String body = objectMapper.writeValueAsString(userLoginDto);
 
+        //when
+        ResultActions result = mockMvc.perform(
+                MockMvcRequestBuilders.post("/user/login")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(body));
+        //then
+        result.andExpect(MockMvcResultMatchers.status().isOk()).andDo(MockMvcResultHandlers.print());
     }
 }
