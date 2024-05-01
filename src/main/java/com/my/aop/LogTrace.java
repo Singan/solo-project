3a8432110c01c5cbf1b9adc9fd54259048afe92e
sunghee119z@naver.com <sunghee119z@naver.com>
2024-05-02 05:01:09 +0900

    로깅 어노테이션 수정 및 테스트

--- src/main/java/com/my/aop/LogTrace.java
+++ src/main/java/com/my/aop/LogTrace.java
@@ -1,5 +1,14 @@
 package com.my.aop;
 
+import io.micrometer.core.instrument.Counter;
+import io.micrometer.core.instrument.Gauge;
+import io.micrometer.core.instrument.MeterRegistry;
+import io.micrometer.core.instrument.Timer;
+import io.micrometer.core.instrument.internal.DefaultGauge;
+import io.micrometer.prometheus.PrometheusMeterRegistry;
+import io.prometheus.client.GaugeMetricFamily;
+import io.prometheus.client.Summary;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -10,29 +19,44 @@ import org.springframework.stereotype.Component;
 @Slf4j
 @Aspect
 @Component
+@RequiredArgsConstructor
 public class LogTrace {
-    public LogTrace() {
-    }
+    private final PrometheusMeterRegistry meterRegistry;
+    private GaugeMetricFamily gaugeMetricFamily = new GaugeMetricFamily("게이지", "메트릭", 100.0);
 
     @Pointcut("@within(com.my.aop.LogClass)")
-    public void logPointcut() {
+    public void apiCounter() {
 
     }
 
-    @Around("logPointcut()")
-    public Object logAround(ProceedingJoinPoint joinPoint) throws Throwable {
+    @Around("apiCounter()")
+    public Object apiStart(ProceedingJoinPoint joinPoint) throws Throwable {
         Object[] args = joinPoint.getArgs();
         long startMills = System.currentTimeMillis();
         log.info("{} 메서드 실행 : {}", joinPoint.getSignature().toShortString(), args);
         Object result = null;
+        // 메서드 실행 시간 측정
+
+        Timer.Sample sample = Timer.start(meterRegistry);
+        
         try {
             result = joinPoint.proceed();
-        }catch (Exception e){
-            log.info("exception 발생 {} " , e);
+            Counter.builder(joinPoint.getSignature().toShortString() + "_counter") // 이름
+                    .tag("class", joinPoint.getSignature().toString()) // 태그1 ( class )
+                    .description(joinPoint.getSignature().toShortString() + " 메서드 실행 : " + args.toString()) // 설명
+                    .register(meterRegistry).increment();
+
+        } catch (Exception e) {
+            log.info("exception 발생 {} ", e);
             throw e;
+        } finally {
+            sample.stop(Timer.builder(joinPoint.getSignature().toShortString() + "_gauge")
+                    .description("소요시간 측정")
+                    .register(meterRegistry));
         }
         long endMills = System.currentTimeMillis() - startMills;
         log.info("{} 메서드 실행 소요 시간 : {}", joinPoint.getSignature(), endMills);
         return result;
     }
+
 }
