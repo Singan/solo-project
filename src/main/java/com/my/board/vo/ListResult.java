9db2b4a8daeffa2c73cd7c32054a779143aa81f8
sunghee119z@naver.com <sunghee119z@naver.com>
2024-07-25 04:51:11 +0900

    Reapply "swagger 작성 v1"
    This reverts commit bdef376becdce69fca5c41896839f548c8d8dd0a.

--- src/main/java/com/my/board/vo/ListResult.java
+++ src/main/java/com/my/board/vo/ListResult.java
@@ -1,6 +1,15 @@
 package com.my.board.vo;
 
-import java.util.List;
+import io.swagger.v3.oas.annotations.media.Schema;
 
-public record ListResult(int pageNo , List<BoardListViewDto> list , boolean prev , boolean next , int leftSize) {
+import java.util.List;
+@Schema(description = "게시글 리스트 조회 결과")
+
+public record ListResult(
+        @Schema(description = "요청한 페이지 넘버") int pageNo,
+        @Schema(description = "요청한 페이지 넘버 결과 리스트")List<BoardListViewDto> list,
+        @Schema(description = "이전 버튼 존재 여부")boolean prev,
+        @Schema(description = "다음 버튼 존재 여부")boolean next,
+        @Schema(description = "남은 글의 개수")int leftSize
+) {
 }
